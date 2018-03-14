45cbaf7a43e6ddfa6a5237b97096161e1505bc17
Viacheslav Kroilov <slavakroilov@gmail.com>
2018-03-14 20:14:01 +0100

    Add MainWindow stub

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,12 @@
 #include <QPushButton>
 #include <QDebug>
 
+#include "forms/mainwindow.h"
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
-    qDebug() << QT_VERSION_STR;
-
-    QPushButton button;
-    button.setText(QT_VERSION_STR);
-    button.setToolTip("A tooltip");
-    button.move(500, 400);
-    button.show();
-
-    QObject::connect(&button, &QPushButton::clicked, &app, QApplication::quit);
-
-
+    MainWindow window;
+    window.show();
     return app.exec();
 }
