ab025f2cfd35f30342372d728c043587c1faf76e
Viacheslav Kroilov <slavakroilov@gmail.com>
2018-03-29 22:06:00 +0200

    Add separate target for test app

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,12 @@
 #include <QApplication>
 #include <QPushButton>
-#include <QDebug>
-#include <include/simple_poisson_solver/poisson.h>
-#include <QtWidgets/QLabel>
-#include <iostream>
 
 #include "forms/mainwindow.h"
 
-using namespace std;
-
 int main(int argc, char** argv) {
-    QApplication app(argc, argv);
+	QApplication app(argc, argv);
 
-    int imgId = 1;
-
-    QImage source;
-    source.load("../src/imgs/source" + QString::number(imgId) + ".png");
-    source = source.scaled(800, 600);
-
-    QImage target;
-    target.load("../src/imgs/target" + QString::number(imgId) + ".jpg");
-    target = target.scaled(800, 600);
-
-    QImage mask;
-    mask.load("../src/imgs/mask" + QString::number(imgId) + ".png");
-    mask = mask.scaled(800, 600);
-
-    cout << "opened" << endl;
-
-    QImage result = simple_solver::poisson(target, source, mask);
-
-    cout << "poissoned" << endl;
-
-    QLabel myLabel;
-    myLabel.setPixmap(QPixmap::fromImage(result));
-    myLabel.setFixedHeight(600);
-    myLabel.setFixedWidth(800);
-
-    myLabel.show();
-
-    result.save("../src/imgs/result" + QString::number(imgId) + ".png");
-
-    cout << "Showed" << endl;
-
-    return app.exec();
+	MainWindow window;
+	window.show();
+	return app.exec();
 }
