e4faf861e4066d95662a20d25a92f1daaa25b9e8
Viacheslav Kroilov <slavakroilov@gmail.com>
2018-05-16 13:40:41 +0200

    Change multicanvas layout

--- src/canvas/multi_canvas_view.cpp
+++ src/canvas/multi_canvas_view.cpp
@@ -11,17 +11,14 @@ MultiCanvasView::MultiCanvasView(CanvasModel& model, QWidget* parent) :
 	auto grid = new QGridLayout;
 	grid->setSizeConstraint(QLayout::SetFixedSize);
 
-	grid->addWidget(new QLabel("Strokes"), 0, 0, Qt::AlignCenter);
-	grid->addWidget(new CanvasView(model, ImageType::IMG_STROKES), 1, 0);
-
-	grid->addWidget(new QLabel("Mask"), 0, 1, Qt::AlignCenter);
-	grid->addWidget(new CanvasView(model, ImageType::IMG_MASK), 1, 1);
+	grid->addWidget(new QLabel("Mask"), 0, 0, Qt::AlignCenter);
+	grid->addWidget(new CanvasView(model, ImageType::IMG_MASK, 1), 1, 0);
 
 	grid->addWidget(new QLabel("Background"), 2, 0, Qt::AlignCenter);
-	grid->addWidget(new CanvasView(model, ImageType::IMG_BG), 3, 0);
+	grid->addWidget(new CanvasView(model, ImageType::IMG_BG, 1), 3, 0);
 
-	grid->addWidget(new QLabel("Composed"), 2, 1, Qt::AlignCenter);
-	grid->addWidget(new CanvasView(model, ImageType::IMG_COMPOSED), 3, 1);
+	grid->addWidget(new QLabel("Composed"), 0, 1, 1, 1, Qt::AlignCenter);
+	grid->addWidget(new CanvasView(model, ImageType::IMG_COMPOSED, 2), 1, 1, 3, 1, Qt::AlignTop);
 
 	setLayout(grid);
 }
